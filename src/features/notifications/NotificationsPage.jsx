ada4a5de6f86ad370298d2c1370f96124ebf3769
uzorchukwutobi-techT <uzorchukwutobi@gmail.com>
2026-08-16 20:18:50 +0100

    Add NotificationsPage component with NotificationsList

---
+++ src/features/notifications/NotificationsPage.jsx
@@ -0,0 +1,10 @@
+import NotificationsList from "./NotificationsList.jsx";
+
+export default function NotificationsPage() {
+  return (
+    <div className="page">
+      <h1>Notifications</h1>
+      <NotificationsList />
+    </div>
+  );
+}
